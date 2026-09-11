d3275a6f38fb7b9cf1d01fe0677fa1dc87660739
GT-NET (ONE AGENT) <saiwinnyeintun2580@gmail.com>
2026-09-11 22:35:18 +0630

    Delete functions directory

--- functions/[[path]].js
+++
@@ -1,7 +0,0 @@
-// Cloudflare Pages Functions Adapter
-// Place this file in functions/[[path]].js in your Pages project
-import worker from "../worker.js";
-
-export const onRequest = (context) => {
-  return worker.fetch(context.request, context.env, context);
-};
